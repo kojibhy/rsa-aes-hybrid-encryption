8379818984c7646fafe1dcee212e02f77cbf3763
kojibhy <kojibhy@gmail.com>
2019-11-28 16:02:11 +0200

    initial

---
+++ src/crypto.py
@@ -0,0 +1,151 @@
+import base64
+import hashlib
+import typing
+
+from Cryptodome import Random
+from Cryptodome.Cipher import AES, PKCS1_OAEP
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Random import get_random_bytes
+
+StringOrBytes = typing.TypeVar('StringOrBytes', str, bytes)  # Must be str or bytes
+
+
+class BaseCipher:
+    block_size: int = 128
+
+    @staticmethod
+    def str_to_bytes(value: [str, bytes]) -> bytes:
+        if isinstance(value, str):
+            return value.encode(encoding='utf8')
+        return value
+
+    def _pad(self, s):
+        return s + (self.block_size - len(s) % self.block_size) * BaseCipher.str_to_bytes(
+            chr(self.block_size - len(s) % self.block_size))
+
+    @staticmethod
+    def _unpad(s):
+        return s[:-ord(s[len(s) - 1:])]
+
+
+class AESCipher(BaseCipher):
+    """AES is a variant of Rijndael which has a fixed block size of 128 bits,
+    and a key size of 128, 192, or 256 bits."""
+
+    def __init__(self, key, *args, **kwargs):
+        self.session_key: bytes = hashlib.sha256(AESCipher.str_to_bytes(key)).digest()
+
+    def encrypt(self, text: StringOrBytes) -> bytes:
+        text: bytes = self.str_to_bytes(text)
+        raw = self._pad(text)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.session_key, AES.MODE_CBC, iv)
+        return iv + cipher.encrypt(raw)
+
+    def decrypt(self, cipher_text: StringOrBytes) -> bytes:
+        """ decode all data to bytes """
+        cipher_text: bytes = self.str_to_bytes(cipher_text)
+        iv: bytes = cipher_text[:AES.block_size]
+        cipher: AES = AES.new(self.session_key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(cipher_text[AES.block_size:]))
+
+    def encrypt_b64(self, text: StringOrBytes, to_string=False) -> StringOrBytes:
+        cipher_text = self.encrypt(text)
+        if to_string:
+            return base64.b64encode(cipher_text).decode()
+        else:
+            return base64.b64encode(cipher_text)
+
+    def decrypt_b64(self, cipher_text: StringOrBytes, to_string=False) -> StringOrBytes:
+        cipher_text: bytes = base64.b64decode(cipher_text)
+        text: bytes = self.decrypt(cipher_text)
+        if to_string:
+            return text.decode()
+        else:
+            return text
+
+
+class RSACipher(BaseCipher):
+    """RSA involves a public key and private key.
+    The public key can be known to everyone; it is used to encrypt messages.
+    Messages encrypted using the public key can only be decrypted with the private key."""
+
+    def __init__(self, import_key):
+        self.rsa_key_pair = RSA.import_key(import_key)
+        super().__init__()
+
+    @classmethod
+    def generate(cls) -> RSA.RsaKey:
+        return RSA.generate(2048)
+
+    @classmethod
+    def generate_keys(cls) -> typing.Tuple[str, str]:
+        rsa_key = RSACipher.generate()
+        rsa_public_key, rsa_private_key = rsa_key.publickey().export_key(), rsa_key.export_key()
+        return rsa_public_key.decode(), rsa_private_key.decode()
+
+    def encrypt(self, text: StringOrBytes, session_key=None) -> typing.Tuple[bytes, bytes]:
+        if not session_key:
+            #   For instance, if you use RSA 2048 and SHA-256, the longest message
+            #             you can encrypt is 190 byte long.
+            session_key: bytes = get_random_bytes(128)
+
+        session_key: bytes = self.str_to_bytes(session_key)
+
+        # Encrypt the session key with the public RSA key
+        cipher_rsa = PKCS1_OAEP.new(
+            self.rsa_key_pair
+        )
+        enc_session_key = cipher_rsa.encrypt(session_key)
+
+        # Encrypt the data with the AES session key
+        aes = AESCipher(key=session_key)
+        return enc_session_key, aes.encrypt(text)
+
+    def decrypt(self, cipher_text: StringOrBytes, session_key: StringOrBytes) -> bytes:
+        # Decrypt the session key with the private RSA key
+        session_key: bytes = self.str_to_bytes(session_key)
+
+        cipher_rsa = PKCS1_OAEP.new(self.rsa_key_pair)
+        session_key = cipher_rsa.decrypt(session_key)
+        # Decrypt the data with the AES session key
+        aes = AESCipher(key=session_key)
+        return aes.decrypt(cipher_text)
+
+    @staticmethod
+    def hashed(text: StringOrBytes):
+        if isinstance(text, str):
+            hashed = hashlib.sha1(text.encode())
+        else:
+            hashed = hashlib.sha1(text)
+        return hashed.hexdigest()
+
+    def encrypt_b64(self, text: StringOrBytes,
+                    session_key=None, to_string=False) -> typing.Dict[str, StringOrBytes]:
+
+        hashed_value: str = self.hashed(text)
+        aes_session_key, cipher_text = self.encrypt(text, session_key)
+
+        if to_string:
+            return dict(
+                session_key=base64.b64encode(aes_session_key).decode(),
+                cipher_text=base64.b64encode(cipher_text).decode(),
+                hashed=hashed_value
+            )
+        else:
+            return dict(
+                session_key=base64.b64encode(aes_session_key),
+                cipher_text=base64.b64encode(cipher_text),
+                hashed=hashed_value.encode()
+            )
+
+    def decrypt_b64(self, cipher_text: StringOrBytes, session_key: StringOrBytes, to_string=False) -> StringOrBytes:
+        session_key: bytes = base64.b64decode(session_key)
+        cipher_text: bytes = base64.b64decode(cipher_text)
+
+        result: bytes = self.decrypt(cipher_text, session_key)
+
+        if to_string:
+            return result.decode()
+        else:
+            return result
---
+++ tests/test_crypto.py
@@ -0,0 +1,277 @@
+import hashlib
+import random
+import string
+import unittest
+
+from Cryptodome.Random import get_random_bytes
+
+from helpers.crypto import AESCipher, StringOrBytes, RSACipher
+
+# Use the system PRNG if possible
+try:
+    random = random.SystemRandom()
+    using_sysrandom = True
+except NotImplementedError:
+    import warnings
+
+    warnings.warn('A secure pseudo-random number generator is not available '
+                  'on your system. Falling back to Mersenne Twister.')
+    using_sysrandom = False
+
+
+def random_string(length=12, allowed_chars=(string.ascii_lowercase
+                                            + string.ascii_uppercase
+                                            + string.digits
+                                            + '!@#$%^&*(-_=+)')):
+    """
+    Return a securely generated random string.
+
+    The default length of 12 with the a-z, A-Z, 0-9 character set returns
+    a 71-bit value. log_2((26+26+10)^12) =~ 71 bits
+    """
+    if not using_sysrandom:
+        # This is ugly, and a hack, but it makes things better than
+        # the alternative of predictability. This re-seeds the PRNG
+        # using a value that is hard for an attacker to predict, every
+        # time a random string is required. This may change the
+        # properties of the chosen random sequence slightly, but this
+        # is better than absolute predictability.
+        random.seed(
+            hashlib.sha256(
+                ('%s%s' % (random.getstate(), time.time())).encode()
+            ).digest()
+        )
+    return ''.join(random.choice(allowed_chars) for i in range(length))
+
+
+class TestCryptoAESHelper(unittest.TestCase):
+
+    def setUp(self):
+        self._test_string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ' \
+                            'incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ' \
+                            'exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure ' \
+                            'dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ' \
+                            'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt ' \
+                            'mollit anim id est laborum. '
+        self._test_bytes_string = self._test_string.encode()
+
+        self.session_key = get_random_bytes(128)
+
+        self._aes = AESCipher(self.session_key)
+
+    def test_text_as_string(self):
+        # test options
+        test_value: str = self._test_string
+        expected_value: bytes = self._test_bytes_string
+        # test case
+        cipher_text: bytes = self._aes.encrypt(test_value)
+        self.assertTrue(isinstance(cipher_text, bytes))
+        text: bytes = self._aes.decrypt(cipher_text)
+        self.assertTrue(isinstance(text, bytes))
+        self.assertEqual(text, expected_value)
+
+    def test_text_as_bytes(self):
+        test_value: bytes = self._test_bytes_string
+        cipher_text: bytes = self._aes.encrypt(test_value)
+        self.assertTrue(isinstance(cipher_text, bytes))
+        text: bytes = self._aes.decrypt(cipher_text)
+        self.assertTrue(isinstance(text, bytes))
+        self.assertEqual(text, test_value)
+
+    def test_text_as_string_to_b64(self):
+        test_value: str = self._test_string
+        expected_value: str = self._test_string
+        cipher_text: StringOrBytes = self._aes.encrypt_b64(
+            test_value,
+            to_string=True
+        )
+        self.assertTrue(isinstance(cipher_text, str))
+        text: str = self._aes.decrypt_b64(
+            cipher_text,
+            to_string=True
+        )
+        self.assertTrue(isinstance(text, str))
+        self.assertEqual(text, expected_value)
+
+    def test_text_as_string_to_b64_to_string(self):
+        # test options
+        test_value: str = self._test_string
+        # test case
+        cipher_text: StringOrBytes = self._aes.encrypt_b64(test_value, to_string=True)
+        self.assertTrue(isinstance(cipher_text, str))
+        text: str = self._aes.decrypt_b64(cipher_text, to_string=True)
+        self.assertTrue(isinstance(text, str))
+        self.assertEqual(text, test_value)
+
+    def test_text_as_bytes_to_b64_to_bytes(self):
+        test_value: bytes = self._test_bytes_string
+        expected_value: bytes = self._test_bytes_string
+        cipher_text: StringOrBytes = self._aes.encrypt_b64(test_value, to_string=False)
+        self.assertTrue(isinstance(cipher_text, bytes))
+        text: bytes = self._aes.decrypt_b64(cipher_text, to_string=False)
+        self.assertTrue(isinstance(text, bytes))
+        self.assertEqual(text, expected_value)
+
+    def test_text_as_bytes_to_b64(self):
+        test_value: bytes = self._test_bytes_string
+        expected_value: bytes = self._test_bytes_string
+        cipher_text: StringOrBytes = self._aes.encrypt_b64(test_value, to_string=False)
+        self.assertTrue(isinstance(cipher_text, bytes))
+        text: bytes = self._aes.decrypt_b64(cipher_text, to_string=False)
+        self.assertTrue(isinstance(text, bytes))
+        self.assertEqual(text, expected_value)
+
+
+class TestCryptoRSAHelper(unittest.TestCase):
+    def setUp(self):
+        self._test_string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ' \
+                            'incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ' \
+                            'exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure ' \
+                            'dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ' \
+                            'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt ' \
+                            'mollit anim id est laborum. '
+
+        self._test_bytes_string = self._test_string.encode()
+
+        self.rsa_public_key, self.rsa_private_key = RSACipher.generate_keys()
+
+    def test_text_as_bytes_with_session_key(self):
+        test_value: bytes = self._test_bytes_string
+        expected_value: bytes = self._test_bytes_string
+
+        aes_key: bytes = b'mysecretkey'
+
+        rsa = RSACipher(self.rsa_public_key)
+        session_key: bytes
+        cipher_text: bytes
+
+        session_key, cipher_text = rsa.encrypt(
+            test_value,
+            session_key=aes_key
+        )
+
+        self.assertTrue(isinstance(session_key, bytes))
+        self.assertTrue(isinstance(cipher_text, bytes))
+
+        rsa = RSACipher(self.rsa_private_key)
+
+        text: bytes = rsa.decrypt(
+            cipher_text,
+            session_key
+        )
+
+        self.assertTrue(isinstance(cipher_text, bytes))
+        self.assertEqual(text, expected_value)
+
+    def test_text_as_string_with_session_key(self):
+        test_value: str = self._test_string
+        expected_value: bytes = self._test_bytes_string
+        aes_key: str = 'mysecretkey'
+
+        rsa = RSACipher(self.rsa_public_key)
+
+        session_key, cipher_text = rsa.encrypt(
+            test_value, session_key=aes_key
+        )
+
+        self.assertTrue(isinstance(session_key, bytes))
+        self.assertTrue(isinstance(cipher_text, bytes))
+
+        rsa = RSACipher(self.rsa_private_key)
+        text: bytes = rsa.decrypt(cipher_text, session_key)
+
+        self.assertTrue(isinstance(cipher_text, bytes))
+        self.assertEqual(text, expected_value)
+
+    def test_text_as_string_random_session_key(self):
+        test_value: str = self._test_string
+        expected_value: bytes = self._test_bytes_string
+
+        rsa = RSACipher(self.rsa_public_key)
+
+        session_key, cipher_text = rsa.encrypt(test_value)
+
+        self.assertTrue(isinstance(session_key, bytes))
+        self.assertTrue(isinstance(cipher_text, bytes))
+
+        rsa = RSACipher(self.rsa_private_key)
+        text: bytes = rsa.decrypt(cipher_text, session_key)
+
+        self.assertTrue(isinstance(cipher_text, bytes))
+        self.assertEqual(text, expected_value)
+
+    def test_text_as_string_random_session_key_asb64(self):
+        test_value: str = self._test_string
+        expected_value: str = self._test_string
+
+        public_rsa = RSACipher(
+            self.rsa_public_key
+        )
+        dictionary = public_rsa.encrypt_b64(
+            test_value,
+            to_string=True
+        )
+        session_key: str = dictionary['session_key']
+        cipher_text: str = dictionary['cipher_text']
+        hashed: str = dictionary['hashed']
+
+        self.assertTrue(isinstance(session_key, str))
+        self.assertTrue(isinstance(cipher_text, str))
+        self.assertTrue(isinstance(hashed, str))
+
+        private_rsa = RSACipher(self.rsa_private_key)
+
+        text: StringOrBytes = private_rsa.decrypt_b64(
+            cipher_text,
+            session_key,
+            to_string=True
+        )
+
+        self.assertTrue(isinstance(text, str))
+        self.assertEqual(text, expected_value)
+
+    def test_text_as_bytes_random_session_key_asb64(self):
+        test_value: bytes = self._test_bytes_string
+        expected_value: bytes = self._test_bytes_string
+
+        public_rsa = RSACipher(self.rsa_public_key)
+
+        dictionary = public_rsa.encrypt_b64(test_value, to_string=False)
+
+        self.assertTrue(isinstance(dictionary['session_key'], bytes))
+        self.assertTrue(isinstance(dictionary['cipher_text'], bytes))
+        self.assertTrue(isinstance(dictionary['hashed'], bytes))
+
+        private_rsa = RSACipher(self.rsa_private_key)
+
+        text: StringOrBytes = private_rsa.decrypt_b64(
+            dictionary['cipher_text'],
+            dictionary['session_key'],
+            to_string=False
+        )
+
+        self.assertTrue(isinstance(text, bytes))
+        self.assertEqual(text, expected_value)
+
+    def test_text_as_bytes_to_b64(self):
+        test_value: bytes = self._test_bytes_string
+        expected_value: bytes = self._test_bytes_string
+        aes_key: str = 'mysecretkey'
+        rsa = RSACipher(self.rsa_public_key)
+
+        dictionary = rsa.encrypt_b64(test_value, session_key=aes_key, to_string=False)
+
+        self.assertTrue(isinstance(dictionary['session_key'], bytes))
+        self.assertTrue(isinstance(dictionary['cipher_text'], bytes))
+        self.assertTrue(isinstance(dictionary['hashed'], bytes))
+
+        rsa = RSACipher(self.rsa_private_key)
+
+        text: StringOrBytes = rsa.decrypt_b64(
+            dictionary['cipher_text'],
+            dictionary['session_key'],
+            to_string=False
+        )
+
+        self.assertTrue(isinstance(text, bytes))
+        self.assertEqual(text, expected_value)
